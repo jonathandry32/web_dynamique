e01576c38ad8131fd447408a9d972644e26cadef
jonathandry32 <119029850+jonathandry32@users.noreply.github.com>
2023-03-07 09:29:11 +0100

    Add files via upload

---
+++ WEB-INF/Files/FrontServlet.java
@@ -0,0 +1,33 @@
+import java.io.*;
+import javax.servlet.*;
+import javax.servlet.http.*;
+import java.util.HashMap;
+
+public class FrontServlet extends HttpServlet {
+    protected void processRequest(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+            response.setContentType("text/plain");
+		    PrintWriter out = response.getWriter();
+            String url=request.getRequestURL().toString();
+            String[] url_=url.split("/");
+            url="";
+            for (int i=5;i<url_.length;i++) {
+                url+=url_[i];
+            }
+            String requete=request.getQueryString();
+            if (requete!=null) {
+                url=url+"?"+requete;
+            }
+            out.println(url);
+    }
+    @Override
+    protected void doGet(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        processRequest(request, response);
+    }
+    @Override
+    protected void doPost(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        processRequest(request, response);
+    }
+}
