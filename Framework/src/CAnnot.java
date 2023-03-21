36a6be8c0bcc439c2614c2558c4ce50e1dd7ff67
jonathandry32 <bayronkevin1@gmail.com>
2023-03-21 11:22:00 +0100

    sprint4

---
+++ Framework/src/CAnnot.java
@@ -0,0 +1,7 @@
+package etu2040.framework.servlet.annotations;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+@Retention(RetentionPolicy.RUNTIME)
+public @interface CAnnot {
+    
+}
